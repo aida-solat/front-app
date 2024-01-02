113827740360d5d6c09723423c5ef6d9845947c8
Aida Solat <aidasolatco@gmail.com>
2024-01-02 16:28:12 +0100

    add some changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           <div className='App'>
             <Routes>
               <Route
-                path='/home'
+                path='/'
                 element={<Home />}
               />
               <Route
